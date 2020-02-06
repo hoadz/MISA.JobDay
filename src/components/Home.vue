12e98de5a5abed7688ee74744faef20e0114c15d
HOA NGUYEN VIET <nvhoa@fresher.local>
2020-02-06 14:56:23 +0700

    Add new project Jobday.vn 06/02/2020 vnhoa

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <header-home></header-home>   
+        <menu1></menu1>
+    </div>
+</template>
+<script>
+import headerHome from './Layout/header.vue'
+import menu1 from './Layout/menu.vue'
+export default {
+    components:{
+        headerHome,
+        menu1
+    }
+}
+</script>
+<style >
+.iconBackground{
+       background-image: url('../assets/icon.svg');
+        
+    }
+</style>
